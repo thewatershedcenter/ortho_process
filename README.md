066aee26d564fbcd9ffbdab1110b398e64a357f5
michael <michaelhuggins@protonmail.com>
2022-07-26 12:16:04 -0700

    added files, skinny works provisionally

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ortho_process
-scripts for processing UAV aquisitions
+# High resolution ortho imagery and pointcloud processing tools #
+
+## UAV ortho processing workflow ##
+1. Run `skinny_tile.py`. It will retile the orthophoto, DSM, and pointcloud into the same tiling scheme.  It will also create a tile index. It does this by reading a slice (from henceforth, and within the scripts comments,  called a _tranche_ in order to avoid confusion with the notion of an array slice in python) of each the orthophoto and the DSM into respective arrays,  then finding the range of valid data within that slice along the axis perpendicular to the tranche.  The extent of the data arrays within the tranche is then adjusted to the new bounds.  Then, for each tranche the arrays are clipped as squares and written to tiles.  The tiles are saved as polygons which are then used to a) create tiled point clouds by reading windows of the input pointcloud (formatted as copc) and b) create the tile index.
+
+It requires the orthophoto, DSM and pointcloud as arguments.  Otionally one may specify the `--tranche_size` argument which determins the thickness of the tranche in map units.  The default tranche thickness is 500. In the case of a high resolution image and map units of meters can be a very large file and may exhaust memory on some machines.  In the future it will be possible to choose which axis the initial slicing will take place, but at the moment it always occurs along the y axis.
+
+The tiled data will be written to their own directories within the directory housing the orthophoto.  If the orthophoto, DSM and pointcloud are called `ortho.tif`, `dsm.tif`, and `points.copc.las`, and are all in the same directory, called `data`,  after running `skinny_tile.py` that directory will have a structure like:
+
+```
+data/
+    |__ortho.tif
+    |__dsm.tif
+    |__points.copc.las
+    |__tindex.gpkg
+    |__tiled_ortho
+    |            |__ortho_0_0.tif
+    |            |__ortho_0_1.tif
+    |            |__ ...
+    |            |__ortho_22_3.tif
+    |__tiled_dsm
+    |            |__dsm_0_0.tif
+    |            |__dsm_0_1.tif
+    |            |__ ...
+    |            |__dsm_22_3.tif
+    |__tiled_points
+                 |__points_0_0.las
+                 |__points_0_1.las
+                 |__ ...
+                 |__points_22_3.las
+```
+
+2. Run `create_dsm.sh`
--- skinny_tile.py
+++ skinny_tile.py
@@ -1,6 +1,7 @@
 #!/bin/python
-# python multi_retile.py --ortho=/media/storage/SFM3/South_Fork_Mountain_Phase_1_orthomosaic.tiff --dsm=/media/storage/SFM3/South_Fork_Mountain_Phase_1_dsm.tiff --copc=/media/storage/SFM3/South_Fork_Mountain_Phase_1-dense_point_cloud.copc.laz
+# python skinny_tile.py --ortho=/media/storage/SFM3/South_Fork_Mountain_Phase_1_orthomosaic.tiff --dsm=/media/storage/SFM3/South_Fork_Mountain_Phase_1_dsm.tiff --copc=/media/storage/SFM3/South_Fork_Mountain_Phase_1-dense_point_cloud.copc.laz --tranche_size=250
 
+#%%
 import threading
 import pdal
 from osgeo import gdal
@@ -76,14 +77,87 @@ def parse_arguments():
     return args
 
 
+def clip_and_write(xmin, xmax, ymin, ymax):
+    # clip to tile
+    ortho_tile = sub_ortho.rio.clip_box(minx=xmin,
+                                        miny=ymin,
+                                        maxx=xmax,
+                                        maxy=ymax)
+
+
+    dsm_tile = dsm.rio.clip_box(minx=xmin,
+                                miny=ymin,
+                                maxx=xmax,
+                                maxy=ymax)
+
+    # make filename for ortho tile
+    dst = os.path.join(ortho_out,
+                    f'_{i}_{j}'.join(os.path.splitext(
+                            os.path.basename(args.ortho))))
+
+    # write ortho tile
+    print(f'writing {dst}')
+    with ProgressBar():
+        ortho_tile.rio.to_raster(dst, lock=threading.Lock())
+
+    # make filename for dsm tile
+    dst = os.path.join(dsm_out,
+                    f'_{i}_{j}'.join(os.path.splitext(
+                            os.path.basename(args.dsm))))
+
+    # write dsm tile
+    print(f'writing {dst}')
+    with ProgressBar():
+        dsm_tile.rio.to_raster(dst, lock=threading.Lock())
+
+
+    # make tindex polygon
+    poly = Polygon(((xmin, ymax),
+                    (xmax, ymax),
+                    (xmax, ymin),
+                    (xmin, ymin),
+                    (xmin, ymax)))
+
+    # append to list for later
+    geometry.append(poly)
+    id.append(f'{i}_{j}')
+
+    # make las tile  name
+    las_tile = f'_{i}_{j}'.join(os.path.splitext(
+                            os.path.basename(args.copc)))
+
+    # convert poly to wkt string
+    poly = wkt.dumps(poly) + f'/ EPSG:{crs}'
+
+    # make pdal pipeline
+    reader = pdal.Reader.copc(args.copc, polygon=poly)
+    ground = pdal.Filter.csf(ignore='Classification[7:7]')
+    writer = pdal.Writer.las(filename=las_tile)
+    pipeline = reader | ground | writer
+
+    # execute pipeline
+    results = pipeline.execute()
+    print(f'{results} points read within')
+    print(poly)
+
+    # move the las file to destination
+    # this is due to bug in pdal, will only write to pwd without
+    # RuntimeError: writers.las: Couldn't open file ... for output.
+    if os.path.isfile(os.path.join(las_out, las_tile)):
+        os.remove(os.path.join(las_out, las_tile))
+    _ = shutil.move(las_tile, las_out)
+
+
+    # here you can exctract the points and do python stuff
+    #points = pipeline.arrays[0]
+    #description = points.dtype.descr
+
 
 if __name__ == '__main__':
     '''This is very procedural!'''
 
-
     # fetch the args
     args = parse_arguments()
-
 
     # open the ortho
     ortho = rioxarray.open_rasterio(
@@ -143,11 +217,11 @@ if __name__ == '__main__':
         xrange = np.arange(xmin, xmax, res, dtype=np.float64)
         yrange = np.arange(ymax, ymin, -res, dtype=np.float64)
 
-        # get the subset in the y slice
+        # get the y tranche
         sub_ortho = ortho.rio.clip_box(minx=xmin,
-                                    miny=ymin,
-                                    maxx=xmax,
-                                    maxy=ymax)
+                                      miny=ymin,
+                                      maxx=xmax,
+                                      maxy=ymax)
 
         # find the first and last occurence of the valid data on tranche axis
         validity = sub_ortho.data[0, ...] > 0
@@ -166,7 +240,7 @@ if __name__ == '__main__':
         lasts = val.argmax(axis=args.axis)
 
         try:
-            last = last = -(lasts[lasts > 0].min() - 1)
+            last = -(lasts[lasts > 0].min() - 1)
         except:
             # in case values go all the way to edge
             last = -1
@@ -176,77 +250,43 @@ if __name__ == '__main__':
             ymin = yrange[first]
             ymax = yrange[last]
         else:
-            xmin = xrange[first]
+            tile_min = xmin = xrange[first]
             xmax = xrange[last]
 
-        # clip subs down to new edges
-        sub_ortho = sub_ortho.rio.clip_box(minx=xmin,
-                                        miny=ymin,
-                                        maxx=xmax,
-                                        maxy=ymax)
+            # for each full tile
+            width = int((xmax - xmin) // tile_size)
+            for j in range(width):
 
-        sub_dsm = dsm.rio.clip_box(minx=xmin,
-                                miny=ymin,
-                                maxx=xmax,
-                                maxy=ymax)
+                # label number
+                j
 
-        # make filename for ortho tile
-        dst = os.path.join(ortho_out,
-                        f'_{i}'.join(os.path.splitext(
-                                os.path.basename(args.ortho))))
+                # calculate max edge of tile
+                tile_max = tile_min + tile_size
+                print()
+                print(f'tile{j + 1} of {width} within tranche {i} of {len(ysteps)}')
+                print(f'tile x bounds:{int(tile_min)}, {int(tile_max)}')
+                print(f'out of: {int(xmax)}, {int(xmax)} for the tranche.')
 
-        # write ortho tile
-        print(f'writing {dst}')
-        with ProgressBar():
-            sub_ortho.rio.to_raster(dst, lock=threading.Lock())
+                # TODO i, j in to clip_and_write to make less confusing
+                # clip the tile and write outputs
+                clip_and_write(tile_min, tile_max, ymin, ymax)
 
-        # make filename for dsm tile
-        dst = os.path.join(dsm_out,
-                        f'_{i}'.join(os.path.splitext(
-                                os.path.basename(args.dsm))))
+                # set new tile min
+                tile_min = tile_max
 
-        # write dsm tile
-        print(f'writing {dst}')
-        with ProgressBar():
-            sub_dsm.rio.to_raster(dst, lock=threading.Lock())
+            if xmax % tile_size > 0:
+                # increment j
+                j = j + 1
 
+                # make tile for the remainder
+                tile_max = xmax
 
-        # make tindex polygon
-        poly = Polygon(((xmin, ymax),
-                        (xmax, ymax),
-                        (xmax, ymin),
-                        (xmin, ymin),
-                        (xmin, ymax)))
+                print()
+                print(j)
+                print(int(xmin), int(tile_min), int(tile_max), int(xmax))
 
-        # append to list for later
-        geometry.append(poly)
-        id.append(i)
-
-        # make las tile  name
-        tmp_las = f'_{i}'.join(os.path.splitext(
-                                os.path.basename(args.copc)))
-
-        # convert poly to wkt string
-        poly = wkt.dumps(poly) + f'/ EPSG:{crs}'
-
-        # make pdal pipeline
-        reader = pdal.Reader.copc(args.copc, polygon=poly)
-        writer = pdal.Writer.las(filename=tmp_las)
-        pipeline = reader | writer
-
-        # execute pipeline
-        results = pipeline.execute()
-        print(f'{results} points read within {poly}')
-
-        # move the las file to destination
-        # this is due to bug in pdal, will only write to pwd without
-        # RuntimeError: writers.las: Couldn't open file ... for output.
-        _ = shutil.move(tmp_las, las_out)
-
-
-        # here you can exctract the points and do python stuff
-        #points = pipeline.arrays[0]
-        #description = points.dtype.descr
+                # clip the tile and write outputs
+                clip_and_write(tile_min, tile_max, ymin, ymax)
 
     gdf = pd.DataFrame(id, columns = ['poly_ID'])
     gdf = gpd.GeoDataFrame(gdf, geometry=geometry)
@@ -256,3 +296,7 @@ if __name__ == '__main__':
     tindex = os.path.join(os.path.dirname(args.ortho), 'tindex.gpkg')
     print(f'writing {tindex}')
     gdf.to_file(tindex)
+
+
+
+
